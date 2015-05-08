c7c3ed09155e8c4c939b5c5dc31af561470ca7f8
Adam Tait <adam@adamtait.com>
2015-05-07 21:15:44 -0700

    Initial Commit

---
+++ tedxtimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tedxtimer
+//
+//  Created by Adam Tait on 5/7/15.
+//  Copyright (c) 2015 Adam Tait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tedxtimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tedxtimer
+//
+//  Created by Adam Tait on 5/7/15.
+//  Copyright (c) 2015 Adam Tait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
